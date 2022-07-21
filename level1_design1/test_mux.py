6887d4844307210128a11ec3594dfb7b6d5962c9
shyamkumarcoc <shyamkumarscoc@gmail.com>
2022-07-21 18:08:56 +0000

    edited

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,10 +7,15 @@ import random
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-    sel = random.randint(0, 31)
-    inp0!=inp1!=inp2!=inp3!=inp4!=inp5!=inp6!=inp7!=inp8!=inp9!=inp10!=inp11!=inp12!=inp13!=inp14!=inp15!=inp16!=inp17!=inp18!=inp19!=inp20!=inp21!=inp22!=inp23!=inp24!=inp25!=inp26!=inp27!=inp28!=inp29!=inp30
 
-    dut.a.value = sel
-    await Timer(2, units='ns')
-    assert dut.result.value == out==inp0!=inp1!=inp2!=inp3!=inp4!=inp5!=inp6!=inp7!=inp8!=inp9!=inp10!=inp11!=inp12!=inp13!=inp14!=inp15!=inp16!=inp17!=inp18!=inp19!=inp20!=inp21!=inp22!=inp23!=inp24!=inp25!=inp26!=inp27!=inp28!=inp29!=inp30[sel], f"mux result is incorrect: {dut.X.value} != 15"
-           
+    for i in range(31):
+
+        sel = random.randint(0, 31)
+        out = random.randint(0, 31)
+
+        dut.a.value = sel
+        dut.b.value = out
+        await Timer(2, units='ns')
+        dut._log.info(f'sel={sel:05} out={out:05} model={sel!=out:05} DUT={int(dut.result.value):05}')
+        assert dut.result.value == sel!=out, "Randomised test failed with: {sel} != {out} = {Result}".format(
+            sel=dut.a.value, out=dut.b.value, Result=dut.result.value)
